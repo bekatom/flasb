b71dd5ceef9c2e64a77299a469736e5f40aee43c
l_beka_l <beka@tomashvili.com>
2014-08-30 18:30:35 +0400

    removed sijax
    removed sijax

--- flaskstarter.py
+++ flaskstarter.py
@@ -2,16 +2,14 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 import os
-import flask_sijax
+
 
 app = Flask(__name__)
-app.config.from_object('config.ProductionConfigHome')
+app.config.from_object('config.ProductionConfig')
 
-path = os.path.join('.', os.path.dirname(__file__), 'static/js/sijax/')
-app.config['SIJAX_STATIC_PATH'] = path
-app.config['SIJAX_JSON_URI'] = '/static/js/sijax/json2.js'
 
-flask_sijax.Sijax(app)
+
+
 
 db = MongoEngine(app)
 
@@ -19,4 +17,4 @@ from views import *
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000, host="localhost")
--- views.py
+++ views.py
@@ -1,108 +1,86 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, request, redirect, render_template, url_for,session, escape,jsonify,g
+from flask import Blueprint, request, redirect, render_template, url_for, session, escape, jsonify, g
 from flask.views import MethodView
 from flask.ext.mongoengine.wtf import model_form
-from flaskstarter import  app
+from flaskstarter import app
 
 
 
 
-#### ERROR HANDLER #########
+# ### ERROR HANDLER #########
 @app.errorhandler(404)
 def page_not_found(error):
-	return render_template('404.html'),404
+    return render_template('404.html'), 404
 
 
-
-@app.route('/',methods=['POST','GET'])
+@app.route('/', methods=['POST', 'GET'])
 @app.route('/')
 def main():
-	if g.sijax.is_sijax_request:
-		call_test_callbacks(g)
-		return g.sijax.process_request()
-	return render_template('index.html')
+    return render_template('index.html')
 
 
-@app.route('/contact',methods=['POST','GET'])
+@app.route('/contact', methods=['POST', 'GET'])
 @app.route('/contact')
 def contact():
-	if request.method =='POST':
-		message = 'This is post message'
-		return render_template('contact.html',message=message)
-	else:
-		return render_template('contact.html')
+    if request.method == 'POST':
+        message = 'This is post message'
+        return render_template('contact.html', message=message)
+    else:
+        return render_template('contact.html')
 
 
 @app.route('/about')
 def about():
-	return render_template('about.html')
+    return render_template('about.html')
 
 
-
-@app.route('/login',methods=['POST','GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-	if request.method =='POST':
-		email = request.form['email']
-		password = request.form['password']
-
-		""" 
-			From mongo database
-			#user = User.objects.get(email = email)
-		"""
-		
-		if email == 'beka@vobi.ge' and password =='qwerty':
-			session['is_auth'] = True
-			session['email'] = email
-			return render_template('index.html',session=session)
-		else:
-			return render_template('login.html',message="Email or password is incorrect, try again")    
-	else:
-		return render_template('login.html')
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
 
 
-@app.route('/registration',methods=['POST','GET'])
+        # From mongo database
+        # #user = User.objects.get(email = email)
+        #
+
+        if email == 'beka@vobi.ge' and password == 'qwerty':
+            session['is_auth'] = True
+            session['email'] = email
+            return render_template('index.html', session=session)
+        else:
+            return render_template('login.html', message="Email or password is incorrect, try again")
+    else:
+        return render_template('login.html')
+
+
+@app.route('/registration', methods=['POST', 'GET'])
 def registration():
-	if request.method == 'POST':
-		email = request.form['email']
-		password = request.form['password']
-
-		"""
-		Registration in Mongo database
-
-		user = User(identification = identification,
-					email= email,
-					password = password
-					)
-		user.save()
-
-		"""
-		return  redirect(url_for('login',message="Registered Success"))
-												 
-	else:
-		return  render_template('registration.html')
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
 
 
-## SIJAX METHODS ############
-def hello_handler(obj_response, hello_from, hello_to):
-	obj_response.alert('Hello from %s to %s' % (hello_from, hello_to))
-	obj_response.css('a', 'color', 'green')
-
-def goodbye_handler(obj_response):
-	obj_response.alert('Goodbye, whoever you are.')
-	obj_response.css('a', 'color', 'red')
-
+        # Registration in Mongo database
+        #
+        # user = User(identification = identification,
+        # email= email,
+        # password = password
+        # 			)
+        # user.save()
 
 
+        return redirect(url_for('login', message="Registered Success"))
 
-def call_test_callbacks(g):
-	g.sijax.register_callback('say_hello', hello_handler)
-	g.sijax.register_callback('say_goodbye', goodbye_handler)
+    else:
+        return render_template('registration.html')
 
 
-## LOGOUT
+# # LOGOUT
 @app.route('/logout')
 def logout():
-	#remove username from session
-	session.pop('is_auth',None)
-	session.pop('email',None)
-	return  redirect(url_for('main'))
+    #remove username from session
+    session.pop('is_auth', None)
+    session.pop('email', None)
+    return redirect(url_for('main'))
